080ba12c4d50934d172be7c8f642de5d791c7c0f
Celestin Lengele <celestin.lengele@macq.eu>
2023-12-01 11:19:44 +0100

    [ADD] add makefile and template for aoc 2023

---
+++ 2023/template.cpp
@@ -0,0 +1,29 @@
+// General useful import
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+// Devlare here global constant and additional function
+
+int main(int argc, char** argv){
+    //declare variables here
+    int result = 0;
+    std::string line;
+    std::ifstream input_file("template.input");
+    if (input_file.is_open()){
+        while(getline(input_file, line)){
+            // add your code for each line here
+        }
+
+        // add your code when finished to read input or after input computation
+
+        //print the result
+        std::cout << "Result is : " << result << std::endl;
+    }
+
+}
